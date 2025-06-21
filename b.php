e0d00a691f6c9defcd4ff69531345117231aa534
AlamgirRejvi <alamgirrejvi65@gmail.com>
2025-06-22 00:01:13 +0600

    new php folder

---
+++ b.php
@@ -0,0 +1,11 @@
+<?php
+echo "Hello, World!<br/>";
+echo "This is a new line <br>";
+
+?>
+
+<?php
+echo "Hello, World!<br/>";
+echo "Welcome to the php world";
+
+?>
---
+++ variable.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+
+    echo "Welcome to the PHP World <br/>";
+    echo "Welcome to the PHP World <br>";
+    echo "Welcome to the PHP World";
+    echo "Welcome to the PHP World";
+    echo "Welcome to the PHP World";
+    
+    echo "hi I am Alamgir Rejvi\r\n";
+    echo "hi I am Alamgir Rejvi <br>";
+    
+    
+    ?>
+
+    <?php
+    echo "Hello, World!\r\n";
+    echo "This is a new line.\r\n";
+?>
+
+<?php
+$text = "Hello everyone, World!\nThis is a new line.";
+echo nl2br($text);
+?>
+</body>
+</html>
