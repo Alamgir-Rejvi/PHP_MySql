0de4c3d51dedd9dff270d966dc159e56e15b81a8
AlamgirRejvi <alamgirrejvi65@gmail.com>
2025-06-22 23:45:05 +0600

    edited variable

--- hello.php
+++ hello.php
@@ -2,7 +2,7 @@
 
 echo "Hello World! </br>"; // This is my first print in php (It's a comment with forword slash)
 echo "I'm here to learn php. <br>"; # here I used Hash tag for comment.
-echo "This is PHP world."; # here I used break tag with closing and without closing tag
+echo "This is The PHP world."; # In above two line, I used break tag with closing and without closing tag
 
 /* echo "Hello World! </br>"; this is also a comment for multiple line...
 echo "Hello World! </br>";
--- variable.php
+++ variable.php
@@ -6,28 +6,65 @@
     <title>Document</title>
 </head>
 <body>
-    <?php 
+<?php 
+// types of variable in php : string, float, integer, binary, hexadecimal, octal, boolean, object, null, resource
 
-    echo "Welcome to the PHP World <br/>";
-    echo "Welcome to the PHP World <br>";
-    echo "Welcome to the PHP World";
-    echo "Welcome to the PHP World";
-    echo "Welcome to the PHP World";
-    
-    echo "hi I am Alamgir Rejvi\r\n";
-    echo "hi I am Alamgir Rejvi <br>";
-    
-    
-    ?>
 
-    <?php
-    echo "Hello, World!\r\n";
-    echo "This is a new line.\r\n";
-?>
 
-<?php
-$text = "Hello everyone, World!\nThis is a new line.";
-echo nl2br($text);
+// string
+$color = "red"; // variable starts with $(dollor sign) and it is case sensitive 
+$Color = "green";
+$COLOR = "blue";
+echo "This is $color color <br>"; // it will output : This is red color.
+echo "This is $Color color <br>"; // it will output : This is green color.
+echo "This is $COLOR color <br>"; // it will output : This is blue color.
+echo "This is " . $COLOR . " color" ; // with dot both side of variable can be declared.
+echo "<br>";
+
+
+
+// float
+$_number = 234.3455; // variable can be declared with underscore sign in any place after $ but not with number
+echo $_number;
+echo "<br>";
+
+$float_number = 444.3455;
+echo $float_number;
+echo "<br>";
+
+$float_number_example = 367.533;
+echo $float_number_example;
+echo "<br>";
+
+
+
+// integer
+$number = 32;
+echo $number;
+echo "<br>";
+
+
+// binary
+$b = 0b101011;
+echo $b // output will be in integer : 43 
+
+
+
+// // hexadecimal
+// $h = 340x123345334325;
+// echo $h;
+
+// octal
+// $oct = 05;
+// echo $oct;
+
+// boolean
+$bool = true;
+echo $bool;
+
+
+// object 
+$obj = 
 ?>
 </body>
 </html>
