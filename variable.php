dcc75b12354ead95ba802c4a9aab529c1ffcdaaa
AlamgirRejvi <alamgirrejvi65@gmail.com>
2025-06-23 22:15:04 +0600

    added variableScope.php

--- variable.php
+++ variable.php
@@ -15,9 +15,9 @@
 $color = "red"; // variable starts with $(dollor sign) and it is case sensitive 
 $Color = "green";
 $COLOR = "blue";
-echo "This is $color color <br>"; // it will output : This is red color.
-echo "This is $Color color <br>"; // it will output : This is green color.
-echo "This is $COLOR color <br>"; // it will output : This is blue color.
+echo "This is $color color <br>"; // output : This is red color.
+echo "This is $Color color <br>"; // output : This is green color.
+echo "This is $COLOR color <br>"; // output : This is blue color.
 echo "This is " . $COLOR . " color" ; // with dot both side of variable can be declared.
 echo "<br>";
 
@@ -25,46 +25,97 @@ echo "<br>";
 
 // float
 $_number = 234.3455; // variable can be declared with underscore sign in any place after $ but not with number
-echo $_number;
+echo $_number; // output : 234.3455
 echo "<br>";
 
 $float_number = 444.3455;
-echo $float_number;
+echo $float_number; // output : 444.3455
 echo "<br>";
 
 $float_number_example = 367.533;
-echo $float_number_example;
+echo $float_number_example; // output : 367.533
 echo "<br>";
 
 
 
 // integer
 $number = 32;
-echo $number;
+echo $number; // output : 32
 echo "<br>";
 
 
 // binary
 $b = 0b101011;
-echo $b // output will be in integer : 43 
+echo $b; // output will be in integer : 43 
+echo "<br>";
 
 
+// hexadecimal
+$h = 0x1F;
+echo $h; // output will be in integer : 31
+echo "<br>";
 
-// // hexadecimal
-// $h = 340x123345334325;
-// echo $h;
+
 
 // octal
-// $oct = 05;
-// echo $oct;
+$oct = 05;
+echo $oct; // output : 5 (octal 05 is decimal 5)
+echo "<br>";
+
 
 // boolean
 $bool = true;
-echo $bool;
+echo $bool; // output : 1
+echo "<br>";
+
 
 
 // object 
-$obj = 
+class Life {
+    public $present = "nai"; //here "public" is a modifier, It can be accessed from anywhere (outside or inside the class)
+}
+$obj = new life(); // here class is called without parameters because the class has no constructor defined.
+echo $obj->present;  // Outputs: nai
+echo "<br>";
+
+
+
+// object with constructor 
+class Car {
+    public $brand;
+
+    public function __construct($brandName) {
+        $this->brand = $brandName;
+    }
+}
+$obj = new Car("Honda");
+echo $obj->brand; // Outputs: Honda
+echo "<br>";
+
+?>
+
+<!-- we can check what type of variable using var_dump() -->
+ <?php
+// octal
+$oct = 05;
+echo $oct; // output : 5 (octal 05 is decimal 5)
+echo "<br>";
+var_dump($oct); // output : int(5) becouse of being converted into decimal , that is an integer.
+echo "<br>";
+var_dump([2, 3, 56]);
+echo "<br>";
+?>
+
+<!-- we can assign the same value to multiple variables in one line: -->
+<?php
+$a = $b = $c = "letter";
+
+echo $a;
+echo "<br>";
+echo $b;
+echo "<br>";
+echo $c;
+
 ?>
 </body>
 </html>
