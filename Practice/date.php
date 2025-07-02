2e1366b92144941fcb8aab8ad68681524c908315
Alamgir Rejvi <alamgirrejvi65@gmail.com>
2025-07-02 13:10:35 +0600

    new

---
+++ Practice/date.php
@@ -0,0 +1,5 @@
+
+<?php
+date_default_timezone_set("Asia/Dhaka"); // Set your timezone
+echo "Current Time: " . date("h:i:s A");
+?>
---
+++ Practice/tryCatch.php
@@ -0,0 +1,22 @@
+
+<?php 
+
+    $a = 10;
+    try{
+        if($a<30){
+            throw new Exception("Valid Number");
+        }
+        else{
+            throw new Exception("invalid Number");
+        }
+
+    }
+    catch(Exception $e){
+        echo " Your error is: ". $e->getMessage() ."<br>";
+        // echo $e->getMessage();
+    }
+    finally{
+        echo "This is finally done my work ";
+    }
+
+?>
