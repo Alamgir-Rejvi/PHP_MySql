5ca07dc2b06cdbb72b62c414142df6d026f6eb79
AlamgirRejvi <alamgirrejvi65@gmail.com>
2025-07-07 07:10:58 +0600

    string

--- Practice/stringOperator.php
+++ Practice/stringOperator.php
@@ -7,6 +7,8 @@
 </head>
 <body>
     <h3>String operator</h3>
+
+    <!-- String Concatenation -->
     <?php
         $a = "Hello ";
         $a .= "This ";
@@ -14,6 +16,14 @@
         echo $a;
         echo "<br>";
     ?>
+
+    <?php
+    $x = "Hello";
+    $y = "World";
+    $z = "$x $y";
+    echo $z;
+    ?>
+
 
     <?php
         $a = "Hello ";
@@ -28,7 +38,64 @@
         $b .= $a . 3 . " is a ";
         $c .= $b . "prime number.";
         echo $c;
-        
+        echo "<br>";
     ?>
+
+    <?php
+        $x = "John";
+        echo "Hello $x"; // output : Hello John
+        // without concatetion; Single quoted strings does not perform such actions, it returns the string like it was written, with the variable name:
+
+        $x = "John";
+        echo 'Hello $x'; // output : Hello $x
+
+        echo strlen("Hello world!");
+        echo str_word_count("Hello world!");
+        echo strpos("Hello world!", "world");
+        
+
+        // Modify string
+        $x = "Hello World!";
+        echo strtoupper($x);
+
+        $x = "Hello World!";
+        echo strtolower($x);
+
+        $x = "Hello World!";
+        echo str_replace("World", "Dolly", $x);
+
+        $x = "Hello World!";
+        echo strrev($x);
+
+        $x = " Hello World! ";
+        echo trim($x); // The trim() removes any whitespace from the beginning or the end:
+
+
+        // The PHP explode() function splits a string into an array
+        $x = "Hello World!";
+        $y = explode(" ", $x); //Use the print_r() function to display the result:
+        print_r($y);
+    ?>
+
+
+    <?php
+        // PHP - Slicing Strings
+        $x = "Hello World!";
+        echo substr($x, 6, 5);//6 is start index, 5 indicates next 5 index
+
+
+        // Slice to the End
+        $x = "Hello World!";
+        echo substr($x, 6); // output : world!
+    ?>
+
+    <?php
+    // Escape Character: An escape character is a backslash \ followed by the character you want to insert.
+    $x = "We are the so-called \"Vikings\" from the north.";
+    echo $x;
+
+    $x = "Hello\nWorld";
+    echo $x; 
+    ?> 
 </body>
 </html>
