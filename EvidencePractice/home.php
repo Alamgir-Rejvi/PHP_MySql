2f720c06688f1a42544379a81eff55c1b4db375f
Alamgir Rejvi <alamgirrejvi65@gmail.com>
2025-07-16 13:16:19 +0600

    fileupload

--- EvidencePractice/home.php
+++ EvidencePractice/home.php
@@ -8,9 +8,10 @@ if (isset($_POST['submit'])) {
     $id = $_POST['id'];
     $name = $_POST['name'];
     $batch = $_POST['batch'];
+    
 
 
-    $object = new Student ($id,$name,$batch);
+    $object = new Student($id,$name,$batch);
     $object->dataStore();
 }
 ?>
@@ -27,11 +28,13 @@ if (isset($_POST['submit'])) {
         ID : <input type="number" name = "id"> <br> <br>
         Name : <input type="text" name = "name"> <br> <br>
         Batch : <input type="number" name = "batch"><br> <br>
+        Email : <input type="email" name = "email"> <br> <br>
         <input type="submit" name = "submit" value = "Submit">
     </form>
 
     <?php
     Student :: display_onscreen();
+
     ?>
 </body>
 </html>
---
+++ test.php
@@ -0,0 +1,14 @@
+<?php 
+$lfc = NULL; 
+echo $lfc; 
+?>
+
+
+<?php
+if (defined("SITE_NAME")) {
+    echo "Constant is defined";
+} else {
+    echo "Constant is not defined";
+}
+
+?>
