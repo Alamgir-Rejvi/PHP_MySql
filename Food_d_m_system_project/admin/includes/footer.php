ee751715dd9bbec85f7e236c3e861ba47bd12759
Alamgir Rejvi <alamgirrejvi65@gmail.com>
2025-08-11 13:08:56 +0600

    new

--- Food_d_m_system_project/admin/includes/footer.php
+++
@@ -1,8 +0,0 @@
-  </div> <!-- end col -->
-</div> <!-- end row -->
-</div> <!-- end container -->
-<footer class="text-center p-3 bg-dark text-white">
-  &copy; <?= date("Y") ?> Food Delivery Admin
-</footer>
-</body>
-</html>
